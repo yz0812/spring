c2a5fb7e0cc670c2d3730358e14cfe15c38c9578
yz <yz_0812@outlook.com>
2022-04-07 23:26:18 +0800

    修改代码

--- spring-boot-netty-push/src/main/java/com/example/nettypush/pubsub/NettyPushMessageBody.java
+++ spring-boot-netty-push/src/main/java/com/example/nettypush/pubsub/NettyPushMessageBody.java
@@ -8,10 +8,25 @@ import java.io.Serializable;
  * @author sixiaojie
  * @date 2020-08-24-13:51
  */
-@Data
 public class NettyPushMessageBody implements Serializable {
 
     private String userId;
 
     private String message;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
